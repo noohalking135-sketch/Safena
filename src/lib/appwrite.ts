98d56d503edb885dbf395adeebef607fc26f96a0
Nooh <noohalking135@gmail.com>
2026-07-27 04:02:09 +0300

    Add files via upload

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,8 +2,7 @@ import { Client, Databases } from "appwrite";
 
 const client = new Client();
 
-// قراءة معرف المشروع من متغيرات البيئة أو وضعه مباشرة هنا بين الأقواس
-const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID 6a658f7200183d84195b
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID || "6a658f7200183d84195b";
 
 client
 .setEndpoint("https://cloud.appwrite.io/v1")
