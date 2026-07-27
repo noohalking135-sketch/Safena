e4d5321c66c904dbc93fc37e714c42868f4bfab5
Nooh <noohalking135@gmail.com>
2026-07-28 01:08:46 +0300

    Update appwrite.ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Databases } from 'appwrite';
 
 export const APPWRITE_ENDPOINT = "https://tor.cloud.appwrite.io/v1";
 export const APPWRITE_PROJECT_ID = "6a658f7200183d84195b";
-export const APPWRITE_DATABASE_ID = "main_db"; // تم تعديله ليتطابق مع قاعدة بياناتك main_db في الصور
+export const APPWRITE_DATABASE_ID = "6a65915e00291cf7f54c"; // تم وضع الـ Database ID الصحيح هنا
 export const ORDERS_TABLE_ID = "orders";
 export const COMPLAINTS_TABLE_ID = "complaints";
 
