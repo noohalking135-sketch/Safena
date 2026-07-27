266993cf8e74b9fc519c9d4d8f6869489c30463f
Nooh <noohalking135@gmail.com>
2026-07-28 00:12:05 +0300

    Add files via upload

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,4 +1,4 @@
-export const APPWRITE_ENDPOINT = "https://cloud.appwrite.io/v1";
+export const APPWRITE_ENDPOINT = "https://tor.cloud.appwrite.io/v1";
 export const APPWRITE_PROJECT_ID = "6a658f7200183d84195b";
 export const APPWRITE_DATABASE_ID = "6a65915e00291cf7f54c";
 export const ORDERS_TABLE_ID = "orders";
