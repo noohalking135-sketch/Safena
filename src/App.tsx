abbebe34e0b2ec04c6c5efc7f96283c0855a3943
Nooh <noohalking135@gmail.com>
2026-07-29 07:15:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './index.css'; 
 import { useState, useEffect, useRef } from "react";
 import { ShoppingBag } from "lucide-react";
 import { HomePage } from "@/components/HomePage";
