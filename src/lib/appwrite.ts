282cd9c225acb2af8bc2c2faf80000ca0ff2d7cc
Nooh <noohalking135@gmail.com>
2026-08-01 21:07:22 +0300

    Update appwrite.ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, Databases } from 'appwrite';
 
 export const APPWRITE_CONFIG = {
   endpoint: 'https://cloud.appwrite.io/v1',
-  projectId: '6a658f7200183d4195b',
+  projectId: '6a658f7200183d8195b',
   databaseId: '6a65915e00291cf7f54c',
   ordersCollectionId: 'orders',
   complaintsCollectionId: 'complaints',
